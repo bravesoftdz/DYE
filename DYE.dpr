379476fe58b103a4c8dfb32c3ab588e935174ead
dennis07 <den.goehlert@t-online.de>
2017-09-08 11:18:16 +0200

    Server framework implementation

--- DYE.dpr
+++ DYE.dpr
@@ -5,11 +5,54 @@ program DYE;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
 
+  MARS.HTTP.Server.Indy,
+  MARS.Core.Engine,
+  MARS.Core.Application,
+
+  DYE.Resources in 'src\DYE.Resources.pas';
+
+type
+  TServer = class
+  private
+    FEngine: TMARSEngine;
+    FServer: TMARShttpServerIndy;
+  public
+    constructor Create;
+    property Server: TMARShttpServerIndy read FServer;
+    property Engine: TMARSEngine read FEngine;
+  end;
+
+{ TServer }
+
+constructor TServer.Create;
+begin
+  FEngine := TMARSEngine.Create;
+  try
+    Engine.Port := 8066;
+    Engine.ThreadPoolSize := 4;
+    Engine.AddApplication('DYE', '/dye',['DYE.Resources.*']);
+    FServer := TMARShttpServerIndy.Create(Engine);
+    try
+      Server.DefaultPort := Engine.Port;
+      Server.Active := True;
+    except
+      Server.Free;
+      raise;
+    end;
+  except
+    Engine.Free;
+    raise;
+  end;
+end;
+
+var
+  Server: TServer;
 begin
   try
-    { TODO -oUser -cConsole Main : Code hier einfügen }
+    Server := TServer.Create;
+    while true do;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
