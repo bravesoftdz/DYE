54d2c2e04b65b66ff8aff9db38aaa8839f8112ab
dennis07 <den.goehlert@t-online.de>
2017-09-08 12:51:34 +0200

    GoogleVision header implementation

--- DYE.dpr
+++ DYE.dpr
@@ -9,6 +9,7 @@ uses
   MARS.HTTP.Server.Indy,
   MARS.Core.Engine,
   MARS.Core.Application,
+
   DYE.Resources in 'src\DYE.Resources.pas',
   DYE.Request.GoogleVision in 'src\DYE.Request.GoogleVision.pas',
   DYE.Request.AmazonLex in 'src\DYE.Request.AmazonLex.pas',
--- src/DYE.Request.GoogleVision.pas
+++ src/DYE.Request.GoogleVision.pas
@@ -2,11 +2,50 @@ unit DYE.Request.GoogleVision;
 
 interface
 
+uses
+  System.SysUtils,
+  System.Net.HttpClientComponent,
+  Vcl.Graphics,
+  Vcl.Imaging.jpeg;
+
 type
-  TDYEGoogleVisionRequest = class
+  EDYEGoogleVisionRequestError = class(Exception);
+
+  TDYEGoogleVisionResponse<T: TGraphic> = record
 
   end;
 
+  TDYEGoogleVisionCustomRequest<T: TGraphic> = class
+  private
+    FClient: TNetHTTPClient;
+    FData: TDYEGoogleVisionResponse<T>;
+  protected
+    property Client: TNetHTTPClient read FClient;
+  public
+    constructor Create(AGraphic: T);
+    destructor Destroy;
+    property Response: TDYEGoogleVisionResponse<T> read FData;
+  end;
+
+  { USE THIS TYPE FROM ABROAD! }
+  TDYEGoogleVisionRequest = TDYEGoogleVisionCustomRequest<TJPEGImage>;
+
 implementation
+
+{ TDYEGoogleVisionRequest }
+
+constructor TDYEGoogleVisionCustomRequest<T>.Create(AGraphic: T);
+begin
+  if not Assigned(AGraphic) then
+  begin
+    raise Exception.Create('Graphic object must not be NIL');
+  end;
+  FClient := TNetHTTPClient.Create(nil);
+end;
+
+destructor TDYEGoogleVisionCustomRequest<T>.Destroy;
+begin
+  Client.Free;
+end;
 
 end.
