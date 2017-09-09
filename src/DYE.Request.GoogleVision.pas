b5f81e5ef152a3c4cf84d059b523f369c15a275b
dennis07 <den.goehlert@t-online.de>
2017-09-09 09:41:18 +0200

    ssdffg

--- src/DYE.Request.GoogleVision.pas
+++ src/DYE.Request.GoogleVision.pas
@@ -162,7 +162,7 @@ function TDYEGoogleVisionRequest.Request(AGraphic: TBytesStream): TStringStream;
         JsonWriter.WriteStartObject;
 
         JsonWriter.WritePropertyName('content');
-        JsonWriter.WriteValue(TEncoding.ANSI.GetString(AGraphic.Bytes));
+        JsonWriter.WriteValue(TEncoding.UTF8.GetString(AGraphic.Bytes));
 
         JsonWriter.WriteEndObject;
 
@@ -207,16 +207,12 @@ function TDYEGoogleVisionRequest.Request(AGraphic: TBytesStream): TStringStream;
 
 var
   Client: TNetHTTPClient;
-  Response: string;
 begin
   Result := TStringStream.Create;
   try
     Client := TNetHTTPClient.Create(nil);
     try
-      Writeln('Request google');
-      Response := Client.Post(TDYEGoogleVisionAPIData.Url, RequestBody, Result)
-                    .ContentAsString();
-      Writeln('Google ready');
+      Client.Post(TDYEGoogleVisionAPIData.Url, RequestBody, Result);
     finally
       Client.Free;
     end;
